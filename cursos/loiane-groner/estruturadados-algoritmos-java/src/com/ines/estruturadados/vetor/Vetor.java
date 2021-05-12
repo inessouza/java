18a3b11d5ff752d5a9eb67fb89a392b2aad59e4a
inesdev <inessouzasp@gmail.com>
2021-05-11 21:06:32 -0300

    Aula 02 - Vetor

---
+++ cursos/loiane-groner/estruturadados-algoritmos-java/src/com/ines/estruturadados/vetor/Vetor.java
@@ -0,0 +1,10 @@
+package com.ines.estruturadados.vetor;
+
+public class Vetor {
+	
+	private String[] elementos;
+	
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+}
