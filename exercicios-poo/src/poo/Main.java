08df3d961444b06c262b147f24d27549a0784712
inesdev <inessouzasp@gmail.com>
2021-05-16 20:43:04 -0300

    Exercícios POO

---
+++ exercicios-poo/src/poo/Main.java
@@ -0,0 +1,12 @@
+package poo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Carro carro = new Carro("Honda", "HR-V", 2021, "Grafite");
+		carro.status();
+		
+		Carro carro2 = new Carro("Ford", "Focus", 2022, "Preto");
+		carro2.status();
+	}
+}
