9fd66d8200ac2118e5b7d0ad2de057e5b2adc719
inesdev <inessouzasp@gmail.com>
2021-05-17 10:58:36 -0300

    Aula 03

---
+++ cursos/estrutura-de-dados-loianegroner/estruturadados-algoritmos-java/src/com/ines/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,17 @@
+package com.ines.estruturadados.vetor.teste;
+
+import com.ines.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+		public static void main(String[] args) {
+	
+			Vetor vetor = new Vetor(2);
+			
+			vetor.adiciona("elemento 1");
+			vetor.adiciona("elemento 2");
+			vetor.adiciona("elemento 2");
+			
+	}
+
+}
