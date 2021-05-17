cc4cd9133b2bfdebe32c254af47213e41246d394
inesdev <inessouzasp@gmail.com>
2021-05-17 10:10:05 -0300

    Aula 04

---
+++ cursos/poo-java-cursoemvideo/Aula4/src/Aula4/Aula4.java
@@ -0,0 +1,11 @@
+package Aula4;
+
+public class Aula4 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta("Nic", "Amarela", 0.4f);
+		c1.status();
+		Caneta c2 = new Caneta("K", "Laranja", 1.5f);
+		c2.status();
+	}
+}
