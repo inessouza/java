6c8da17931371b16d02e0f62fa5f80e00e9b5227
inesdev <inessouzasp@gmail.com>
2021-05-25 22:18:19 -0300

    Exercício 03

---
+++ conceitos/loops/src/ex03/Intervalos.java
@@ -0,0 +1,13 @@
+package ex03;
+
+public class Intervalos {
+
+	public static void main(String[] args) {
+		for(int i = 0; i <= 2; i++) {
+			System.out.println("Intervalo Minutos: " + i);
+			for(int j = 0; j<=59; j++) {
+				System.out.println("Intervalo Segundos: " + j);
+			}
+		}
+	}
+}
