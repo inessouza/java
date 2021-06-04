bb26804b1c3f51086b1d9c46cc8d8ee321e0e5d7
inesdev <inessouzasp@gmail.com>
2021-06-04 15:10:57 -0300

    Aula 8 - Agregação entre Objetos com Java

---
+++ cursos/poo-java-cursoemvideo/Aula8/src/aula8/UltraEmojiCombat.java
@@ -0,0 +1,32 @@
+package aula8;
+
+public class UltraEmojiCombat {
+
+	public static void main(String[] args) {
+		
+		Lutador l[] = new Lutador[6];
+		l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 
+								68.9f, 11, 2, 1);
+		
+		l[1] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 
+								119.3f, 13, 0, 2);
+		
+		l[2] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 
+								105.7f, 12, 2, 4);
+		
+		l[3] = new Lutador("Putscript", "Brasil", 29, 1.68f, 
+								57.8f, 14, 2, 3);
+		
+		l[4] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 
+								80.9f, 12, 2, 1);
+		
+		l[5] = new Lutador("Dead Code", "Australia", 37, 1.70f, 
+								119.3f, 5, 4, 3);
+		
+		Luta UEC01 = new Luta();
+		UEC01.marcarLuta(l[1], l[5]);
+		UEC01.lutar();
+		l[1].status();
+		l[5].status();
+	}
+}
