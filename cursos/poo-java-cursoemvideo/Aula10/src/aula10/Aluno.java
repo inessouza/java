1f1efaa402ce8046731fc417f1d8efc9a93a8297
inesdev <inessouzasp@gmail.com>
2021-06-09 07:48:31 -0300

    Aula 10 - Herança

---
+++ cursos/poo-java-cursoemvideo/Aula10/src/aula10/Aluno.java
@@ -0,0 +1,26 @@
+package aula10;
+
+public class Aluno extends Pessoa {
+	private int mat;
+	private String curso;
+	
+	public void cancelarMatr() {
+		System.out.println("Matricula será cancelada");
+	}
+
+	public int getMat() {
+		return mat;
+	}
+
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+}
