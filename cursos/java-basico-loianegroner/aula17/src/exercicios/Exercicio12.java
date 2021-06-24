d3d952a7af677408ea81497e610855879a0cc3be
inesdev <inessouzasp@gmail.com>
2021-06-24 19:17:40 -0300

    Exercícios While, Do-While e For

--- cursos/java-basico-loianegroner/aula17/src/exercicios/Exercicio12.java
+++ cursos/java-basico-loianegroner/aula17/src/exercicios/Exercicio12.java
@@ -1,10 +1,24 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Desenvolva um gerador de tabuada, capaz de gerar a tabuada
+		// de qualquer número inteiro entre 1 a 10. O usuário deve informar
+		// de qual número ele deseja ver a tabuada:
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Digite o número o qual você deseja ver a tabuada: ");
+		int numero = scanner.nextInt();
+		
+		int produto = 0;
+		for (int i = 1; i <= 10; i++) {
+			produto = numero * i;
+			System.out.println(numero + " X " + i + " = " + produto);
+		}
+		scanner.close();
 	}
-
 }
