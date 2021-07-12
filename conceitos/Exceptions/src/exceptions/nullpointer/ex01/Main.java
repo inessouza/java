2b1aabc11badee4ab15a10c40a311a93e820d77f
inessouza <inessouzasp@gmail.com>
2021-07-11 23:49:00 -0300

    Exceptions - NullPointer

--- conceitos/Exceptions/src/exceptions/nullpointer/ex01/Main.java
+++ conceitos/Exceptions/src/exceptions/nullpointer/ex01/Main.java
@@ -3,7 +3,7 @@ package exceptions.nullpointer.ex01;
 public class Main {
 
 	public static void main(String[] args) {
-		//Objeto não tem uma referência
+		// Objeto não tem uma referência
 		Address address = null;
 		
 		// Será gerado um nullpointer exception porque o objeto é nulo
